8294153a592df938050c7927a9350ee62d3d23ce
Daxx <64298070+Daxxboii@users.noreply.github.com>
2022-12-01 19:44:38 +0530

    init

---
+++ SnackAttack Multiregion Test/main.tf
@@ -0,0 +1,85 @@
+
+module "cdn-us-east-1" {
+  source = "./Main"
+  region = "us-east-1"
+
+}
+/*
+module "cdn-us-east-2" {
+  source = "./Main"
+  region = "us-east-2"
+}
+
+module "cdn-us-west-1" {
+  source = "./Main"
+  region = "us-west-1"
+}
+
+module "cdn-us-west-2" {
+  source = "./Main"
+  region = "us-west-2"
+}
+
+module "cdn-ca-central-1" {
+  source = "./Main"
+  region = "ca-central-1"
+}
+
+module "cdn-eu-west-1" {
+  source = "./Main"
+  region = "eu-west-1"
+}
+
+module "cdn-eu-west-2" {
+  source = "./Main"
+  region = "eu-west-2"
+}
+
+module "cdn-eu-west-3" {
+  source = "./Main"
+  region = "eu-west-3"
+}
+
+module "cdn-eu-central-1" {
+  source = "./Main"
+  region = "eu-central-1"
+}
+
+
+
+module "cdn-eu-north-1" {
+  source = "./Main"
+  region = "eu-north-1"
+}
+
+module "cdn-ap-northeast-1" {
+  source = "./Main"
+  region = "ap-northeast-1"
+}
+
+module "cdn-ap-northeast-2" {
+  source = "./Main"
+  region = "ap-northeast-2"
+}
+
+module "cdn-ap-southeast-1" {
+  source = "./Main"
+  region = "ap-southeast-1"
+}
+
+module "cdn-ap-southeast-2" {
+  source = "./Main"
+  region = "ap-southeast-2"
+}
+
+module "cdn-ap-south-1" {
+  source = "./Main"
+  region = "ap-south-1"
+}
+
+module "cdn-sa-east-1" {
+  source = "./Main"
+  region = "sa-east-1"
+}
+
+*/
